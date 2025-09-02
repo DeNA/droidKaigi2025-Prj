bd39413afe7d6fb28fffc934d9c63a57b8a04108
Rokichi <rokichi0906@gmail.com>
2025-09-02 19:21:32 +0900

    initial commit

---
+++ app/src/main/java/jp/co/dena/droidkaigi2025_prj/DroidKaigi2025PrjApplication.kt
@@ -0,0 +1,7 @@
+package jp.co.dena.droidkaigi2025_prj
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DroidKaigi2025PrjApplication : Application()
---
+++ app/src/main/java/jp/co/dena/droidkaigi2025_prj/MainActivity.kt
@@ -0,0 +1,48 @@
+package jp.co.dena.droidkaigi2025_prj
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import jp.co.dena.droidkaigi2025_prj.ui.theme.DroidKaigi2025PrjTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DroidKaigi2025PrjTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DroidKaigi2025PrjTheme {
+        Greeting("Android")
+    }
+}
