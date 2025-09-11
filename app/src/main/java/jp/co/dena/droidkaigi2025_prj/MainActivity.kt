8fe675ef7b7d295e92666928d96d058c23022420
sokume <sokume2106@gmail.com>
2025-09-11 11:20:34 +0900

    tacks contribute

--- app/src/main/java/jp/co/dena/droidkaigi2025_prj/MainActivity.kt
+++ app/src/main/java/jp/co/dena/droidkaigi2025_prj/MainActivity.kt
@@ -1,16 +1,27 @@
 package jp.co.dena.droidkaigi2025_prj
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import jp.co.dena.droidkaigi2025_prj.ui.theme.DroidKaigi2025PrjTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,12 +35,47 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+
+
                 }
             }
         }
     }
 }
 
+@Composable
+fun TableItem() {
+
+    // TODO: うまくデータ入れる :pray:
+
+    val items = List(1) {
+        "Nawhal patch 1.1"
+    }
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(64.dp)
+    ) {
+        items.map { sessionItem ->
+            Text(sessionItem,
+                style = TextStyle.Default.copy(
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                )
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewTableItem() {
+    MaterialTheme {
+        TableItem()
+    }
+}
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -39,10 +85,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DroidKaigi2025PrjTheme {
-        Greeting("Android")
-    }
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    DroidKaigi2025PrjTheme {
+//        Greeting("Android")
+//    }
+//}
