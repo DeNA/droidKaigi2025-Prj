faaf7b8a56d685d61051f4667e861cc6186256d3
sokume <sokume2106@gmail.com>
2025-09-12 16:18:47 +0900

    Add vertical scroll to detail screen

--- app/src/main/java/jp/co/dena/droidkaigi2025_prj/ui/timetable/screens/timetabledetail/TimetableDetailScreen.kt
+++ app/src/main/java/jp/co/dena/droidkaigi2025_prj/ui/timetable/screens/timetabledetail/TimetableDetailScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -69,7 +71,8 @@ fun TimetableDetailScreen(
                 Column(
                     modifier = Modifier
                         .padding(innerPadding)
-                        .padding(horizontal = 24.dp),
+                        .padding(horizontal = 24.dp)
+                        .verticalScroll(rememberScrollState()),
                     verticalArrangement = Arrangement.spacedBy(16.dp)
                 ) {
                     Text(
